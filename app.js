02e1419179ed0658e32abe34ac75f2d60d4c0410
dogan.irmak <dogan.irmak@icloud.com>
2022-07-16 03:13:50 +0300

    commit

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ function convertToF(celsius) {
     }
     return reversedStr;
   }
+
+
+  function factorialize(num) {
+    let product = 1;
+    for (let i = 2; i <= num; i++) {
+      product *= i;
+    }
+    return product;
+  }
+  
+  factorialize(5);
