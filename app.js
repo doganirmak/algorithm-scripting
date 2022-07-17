915770b3e195ee7966284db6b2a81d16cbd8bd9a
dogan.irmak <dogan.irmak@icloud.com>
2022-07-18 02:05:58 +0300

    longest word

--- app.js
+++ app.js
@@ -25,3 +25,16 @@ function convertToF(celsius) {
   }
   
   factorialize(5);
+
+  function findLongestWordLength(str) {
+    let words = str.split(' ');
+    let maxLength = 0;
+  
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > maxLength) {
+        maxLength = words[i].length;
+      }
+    }
+  
+    return maxLength;
+  }
