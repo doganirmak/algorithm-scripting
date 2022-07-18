649e2ffae442e0e27d95cdb6e6b4cdf80460e939
dogan.irmak <dogan.irmak@icloud.com>
2022-07-19 02:00:10 +0300

    commit

--- app.js
+++ app.js
@@ -38,3 +38,18 @@ function convertToF(celsius) {
   
     return maxLength;
   }
+
+  function largestOfFour(arr) {
+    const results = [];
+    for (let i = 0; i < arr.length; i++) {
+      let largestNumber = arr[i][0];
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > largestNumber) {
+          largestNumber = arr[i][j];
+        }
+      }
+      results[i] = largestNumber;
+    }
+  
+    return results;
+  }
